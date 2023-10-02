3e5c890de43a59dfbaed79af7f9e27472c1a0488
Bradley Booth <bradley.booth@hotmail.co.uk>
2023-10-02 17:01:34 +0100

    Task 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+function removeFromArray(arr, ...argsToRemove) {
+  const resultArray = [];
+  for (let item of arr) {
+    if (!argsToRemove.includes(item)) {
+      resultArray.push(item);
+    }
+  }
+  return resultArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
